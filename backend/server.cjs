53e464224dd4fd12cecb3f23e31cf2d961f21776
hrIthika21 <kskhrithika@gmail.com>
2024-07-11 10:05:45 +0530

    final changes

---
+++ backend/server.cjs
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express= require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.port || 5002;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port,()=>{
+    console.log(`Server is running on ${port}`)
+})
+
